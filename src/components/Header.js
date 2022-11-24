d8034961fec104c9552a5f264ed39ccd77693f70
RomainNozay <romain.nozay@gmail.com>
2022-11-24 12:03:20 +0100

    Importation des logements

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 const Header = () => {
-    return (
+    const JSX = (
         <div className='header-container'>
             <h1>Je suis le header</h1>
             <p>1 REM = 10 pixel</p>
             <i className="fa-solid fa-magnifying-glass"></i>
         </div>
     );
+
+    return JSX;
 };
 
 export default Header;
