d308ad1f46e7b632c32540ec12291d3544f06d5f
RomainNozay <romain.nozay@gmail.com>
2022-11-24 10:51:41 +0100

    Mise ne place
    Création des dossiers et fichier nécessaire
    installation de Sass
    installation des polices de caractère pour le site

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       
           Learn React
+          <Header />
     </div>
        
   );
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='header-container'>
+            <h1>Je suis le header</h1>
+            <p>1 REM = 10 pixel</p>
+            <i className="fa-solid fa-magnifying-glass"></i>
+        </div>
+    );
+};
+
+export default Header;
