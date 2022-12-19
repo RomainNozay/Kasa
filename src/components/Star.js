9af2e875155072e4fbe995c7660371a7055196ce
RomainNozay <romain.nozay@gmail.com>
2022-12-19 08:55:06 +0100

    erreur de graphie

--- src/components/Star.js
+++ src/components/Star.js
@@ -1,20 +1,19 @@
 import React from "react";
 import PropTypes  from "prop-types";
 
-import redstar from "../asset/img/Etoile.png";
-import greystar from "../asset/img/EtoileVide.png";
+import Redstar from "../asset/img/Etoile.png";
+import Greystar from "../asset/img/EtoileVide.png";
 
 function Rating({ rating })  {
   const starsArray = [1, 2, 3, 4, 5];
 
   return (
     <div className="rating">
-      {/* Loop to display star by rating */}
       {starsArray.map((star) =>
         rating >= star ? (
-          <img key={star.toString()} className="rating_icon" src={redstar} alt="Etoile orangé représentant une note positive sur 5 étoiles"/>
+          <img key={star.toString()} className="rating_icon" src={Redstar} alt="Etoile orangé représentant une note positive sur 5 étoiles"/>
         ) : (
-          <img key={star.toString()} className="rating_icon" src={greystar} alt= "Etoile grisée représentant une point négatif sur 5 étoiles"/>
+          <img key={star.toString()} className="rating_icon" src={Greystar} alt= "Etoile grisée représentant une point négatif sur 5 étoiles"/>
         )
       )}
     </div>
